0ffc4235ae0cf74ac8f8719c5e766849ee7af828
anshu <anshu@10.0.0.193>
2018-11-03 15:03:50 -0400

    Added variables to site class

--- src/edu/nyu/cs/distributedsystem/Site.java
+++ src/edu/nyu/cs/distributedsystem/Site.java
@@ -1,5 +1,16 @@
+
 package edu.nyu.cs.distributedsystem;
 
 class Site {
+	private:
+		enum siteStatus 
+	    { 
+	        DOWN,RECOVERING, UP; 
+	    }
+
+		Map<int,Variable> varMap; 
+		
+		bool processTransaction(int transactionType, Variable var);
+		
 
 }
