33ed0ccc47a1abf90388dbe0df65de7e6c157bc8
Amala <asd508@nyu.edu>
2018-11-03 15:11:45 -0400

    Variable class first draft

--- src/edu/nyu/cs/distributedsystem/Variable.java
+++ src/edu/nyu/cs/distributedsystem/Variable.java
@@ -2,4 +2,37 @@ package edu.nyu.cs.distributedsystem;
 
 class Variable {
 
+  private int index;
+  private int val;
+  private boolean writelock;
+
+  Variable(int i, int v) {
+    index = i;
+    val = v;
+    writelock = false;
+  }
+
+  int getIndex() {
+    return index;
+  }
+
+  int getVal() {
+    return val;
+  }
+
+  void setVal(int v) {
+    val = v;
+  }
+
+  boolean isLocked() {
+    return writelock;
+  }
+
+  void lockVariable() {
+    writelock = true;
+  }
+
+  void unlockVariable() {
+    writelock = false;
+  }
 }
